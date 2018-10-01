aba99e452f8c5513221be13c0bc12221b18856d2
Agung Nugroho <mail@chung.web.id>
2018-10-01 09:56:06 +0700

    tambah latihan1.cpp: program mengitung luas segitiga

---
+++ latihan1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+int main() {
+   int A, T;
+   float L;
+   
+   A = 5;
+   T = 7;
+   
+   L = A * T / 2.0;
+   
+   std::cout << "Program Menghitung Luas Segitia" << std::endl;
+   std::cout << "Alas segitiga = " << A << std::endl;
+   std::cout << "Tinggi segita = " << T << std::endl; 
+   std::cout << "Luas segitiga adalah = " << L << std::endl;
+
+}
